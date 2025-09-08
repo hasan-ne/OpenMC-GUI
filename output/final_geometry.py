c8accd2f5839c2a9117c3ef45382da9d15a69d5e
Mahmudul Hasan <mahmudul.du.ne8@gmail.com>
2025-09-08 19:25:03 +0600

    Delete output directory

--- output/final_geometry.py
+++
@@ -1,6 +0,0 @@
-import openmc
-from geometry import *  # import all previously defined universes/cells
-
-geometry = openmc.Geometry(universes['root_univere'])
-geometry.export_to_xml('output/geometry.xml')
-print('Final geometry created with universe: root_univere')
--- output/openmc_run.py
+++
@@ -1,3 +0,0 @@
-import openmc
-openmc.config['cross_sections'] = r'/home/kali/Documents/openmc/openmcDataset/endfb-viii.0-hdf5/cross_sections.xml'
-openmc.run()
